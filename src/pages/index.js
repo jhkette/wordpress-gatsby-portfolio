3c099b568b4c6b765016f20298199c03c5561bfd
khenryjoe <joseph.ketterer@gmail.com>
2019-07-17 15:50:48 +0100

    change link

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Img from "gatsby-image";
 import Masonry from "react-masonry-component";
 import Layout from "../components/Layout";
 import Code from "../images/code.svg";
-// import Circle from "../components/circles.js";
 import { Helmet } from "react-helmet";
 import xss from "xss";
 
@@ -19,7 +18,6 @@ class Home extends Component {
           <title>Portfolio: Joseph Ketterer</title>
         </Helmet>
 
-        {/* <Circle /> */}
         <Masonry className="container-projects-posts" aria role="main">
           {data.edges.map(({ node }) => (
             <article key={node.slug} className="masonry-item-post">
